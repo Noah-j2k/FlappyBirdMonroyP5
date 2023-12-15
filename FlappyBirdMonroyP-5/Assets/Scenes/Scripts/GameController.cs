cd5c924bd30f7044a69a87efb737448ef799ea42
athanacios <noahmonroy17@gmail.com>
2023-12-14 16:02:42 -0800

    ScrollSpeed
    Vid 7 timestamp: 5:50

--- FlappyBirdMonroyP-5/Assets/Scenes/Scripts/GameController.cs
+++ FlappyBirdMonroyP-5/Assets/Scenes/Scripts/GameController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollspeed = -1.5f;
 
     // Start is called before the first frame update
     void Awake ()
@@ -21,8 +23,12 @@ public class GameController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
-        
+    void Update()
+    {
+        if(gameOver == true && Input.GetMouseButtonDown  (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     public void BirdDied()
