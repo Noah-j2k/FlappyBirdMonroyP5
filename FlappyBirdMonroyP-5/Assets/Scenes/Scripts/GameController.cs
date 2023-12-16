d63dfc0c03ed8d26bd43d7d0fd407aeb2ca2e63d
athanacios <noahmonroy17@gmail.com>
2023-12-16 13:09:35 -0800

    Finished GAME!!!
    Record vids at HOME!!!!!

--- FlappyBirdMonroyP-5/Assets/Scenes/Scripts/GameController.cs
+++ FlappyBirdMonroyP-5/Assets/Scenes/Scripts/GameController.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController instance;
     public GameObject gameOverText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI Continue;
     public bool gameOver = false;
     public float scrollspeed = -1.5f;
 
+    private int score = 0;
+
+    AudioSource audioSource;
+    public AudioClip scoreSound;
+
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void Awake ()
     {
         if (instance == null)
@@ -31,10 +45,26 @@ public class GameController : MonoBehaviour
         }
     }
 
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString ();
+        PlaySound(scoreSound);
+    }
+
     public void BirdDied()
     {
       gameOverText.SetActive(true);
       gameOver = true;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
 
